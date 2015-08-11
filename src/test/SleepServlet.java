133c72243a8d80515a2f2003c335e7a1ce6834a5
ZHANGJA4 <JAMES.ZHANG@OOCL.COM>
2015-08-11 16:59:16 +0800

    Initial commit

---
+++ src/test/SleepServlet.java
@@ -0,0 +1,30 @@
+package test;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by ZHANGJA4 on 8/11/2015.
+ */
+@WebServlet(name = "SleepServlet",urlPatterns = {"/SleepServlet"})
+public class SleepServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String uname = request.getParameter("uname");
+        System.out.println(uname);
+        System.out.println("SleepServlet");
+        PrintWriter out = response.getWriter();
+        out.write("Hello world");
+        out.flush();
+        out.close();
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request,response);
+    }
+}
